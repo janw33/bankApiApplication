d46f4d5621173ddb7a2e9a9df63077f5cf67affe
Jan Wypych <32uczen32@gmail.com>
2026-05-13 16:59:34 +0200

    Feat: Add connection to db

---
+++ src/test/java/com/janwypych/bankApiApplication/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janwypych.bankApiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
