80bf9697dabe52bccf4536d9388d2fa919f4c0df
Jan Wypych <32uczen32@gmail.com>
2026-05-13 19:06:35 +0200

    Add testThatCreateAccountReturnHttp201

---
+++ src/test/java/com/janwypych/bankApiApplication/TestDataUtil.java
@@ -0,0 +1,24 @@
+package com.janwypych.bankApiApplication;
+
+import com.janwypych.bankApiApplication.Dto.CreateAccountRequest;
+import com.janwypych.bankApiApplication.entities.AccountEntity;
+
+public final class TestDataUtil {
+
+    public static AccountEntity createAccountEntity() {
+        return AccountEntity.builder()
+                .firstName("Jan")
+                .lastName("Wypych")
+                .email("janWypych@email.com")
+                .password("password")
+                .build();
+    }
+    public static CreateAccountRequest createCreateAccountRequest() {
+        return CreateAccountRequest.builder()
+                .firstName("Jan")
+                .lastName("Wypych")
+                .email("janWypych@email.com")
+                .password("password")
+                .build();
+    }
+}
---
+++ src/test/java/com/janwypych/bankApiApplication/controllers/AccountControllerIntegrationTests.java
@@ -0,0 +1,48 @@
+package com.janwypych.bankApiApplication.controllers;
+
+import com.janwypych.bankApiApplication.Dto.CreateAccountRequest;
+import com.janwypych.bankApiApplication.TestDataUtil;
+import com.janwypych.bankApiApplication.entities.AccountEntity;
+import com.janwypych.bankApiApplication.services.AccountService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import tools.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+public class AccountControllerIntegrationTests {
+    private final AccountService accountService;
+    private final MockMvc mockMvc;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public AccountControllerIntegrationTests(AccountService accountService, MockMvc mockMvc) {
+        this.accountService = accountService;
+        this.mockMvc = mockMvc;
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testThatCreateAccountReturnHttp201() throws Exception {
+        CreateAccountRequest createAccountRequest = TestDataUtil.createCreateAccountRequest();
+        String accountJson = objectMapper.writeValueAsString(createAccountRequest);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(accountJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        );
+    }
+}
